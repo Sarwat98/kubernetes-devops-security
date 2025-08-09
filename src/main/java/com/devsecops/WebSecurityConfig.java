d0f7eeeb097077cbef9c0413e797948b9cb7a3a1
farisali21 <74044363+farisali21@users.noreply.github.com>
2025-08-09 11:34:28 +0300

    adding the websecurity config class to solve the x-content-type-options

---
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.devsecops;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .headers()
+            .defaultsDisabled()
+            .addHeaderWriter((request, response) -> 
+                response.addHeader("X-Content-Type-Options", "nosniff")) // Use string header name directly
+            .and()
+            .csrf().disable() // Disable CSRF protection
+            .authorizeRequests()
+            .anyRequest().authenticated(); // Customize according to your needs
+    }
+}
