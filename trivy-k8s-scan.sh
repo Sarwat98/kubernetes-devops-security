6af4f554417f17807e0f5fc9c4a198ad648c4410
Sarwat98 <mahmoudtharwat98@outlook.com>
2025-09-10 23:51:19 +0300

    Update trivy-k8s-scan.sh

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
-# trivy-k8s-scan.sh
-
-# Getting Image name from env variable
-echo $imageName
-
-# Scan for LOW, MEDIUM, HIGH severity vulnerabilities
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
-
-# Scan for CRITICAL severity vulnerabilities and set exit code if found
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity CRITICAL --light $imageName
-
-# Capture the exit code from the last scan
-exit_code=$?
-
-# Check scan results
-if [[ ${exit_code} == 1 ]]; then
-  echo "Image scanning failed. Vulnerabilities found"
-  exit 1
-else
-  echo "Image scanning passed. No vulnerabilities found"
-fi
+trivy fs --security-checks config --severity HIGH,CRITICAL \
+    --format table k8s_deployment_service.yaml
+    
+trivy fs --security-checks config --severity HIGH,CRITICAL \
+    --format json --output trivy-k8s-results.json k8s_deployment_service.yaml
